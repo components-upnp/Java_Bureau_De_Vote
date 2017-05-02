8b577b4e885b255adbef3458af6cb5d91affd2fb
MKostiuk <Mat01061995>
2017-05-02 16:41:33 +0200

    Conception prototype interface graphique

--- src/main/java/com/irit/main/App.java
+++ src/main/java/com/irit/main/App.java
@@ -1,5 +1,7 @@
 package com.irit.main;
 
+import upnp.BureauDeVoteServer;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       new Thread(new BureauDeVoteServer()).run();
     }
 }
